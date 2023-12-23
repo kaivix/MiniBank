6fcb37e746898677c0e7d886bfe9db04f2abaa0e
kaivix <74367464+kaivix@users.noreply.github.com>
2023-12-23 16:07:04 +0400

    fix bugs

--- src/main/java/com/kaivix/mini_bank/Controllers/ProfileController.java
+++ src/main/java/com/kaivix/mini_bank/Controllers/ProfileController.java
@@ -1,15 +1,33 @@
 package com.kaivix.mini_bank.Controllers;
 
+import com.kaivix.mini_bank.Models.Users;
+import com.kaivix.mini_bank.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RestController("/user")
+@RequestMapping("/user")
 public class ProfileController {
 
-    @GetMapping("/registration")
-    private String registration(){
-        return "registration";
+    private final UserService userService;
+    @Autowired
+    public ProfileController(UserService userService) {
+        this.userService = userService;
     }
+
+
+    @GetMapping("/reg")
+    public String reg(Model model){
+        model.addAttribute("users", new Users());
+        return "regis";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("users") Users users) {
+        userService.save(users);
+        return "redirect:/";
+    }
+
 }
--- src/main/java/com/kaivix/mini_bank/MiniBankApplication.java
+++ src/main/java/com/kaivix/mini_bank/MiniBankApplication.java
@@ -2,12 +2,18 @@ package com.kaivix.mini_bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class MiniBankApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MiniBankApplication.class, args);
+    }
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
     }
 
 }
