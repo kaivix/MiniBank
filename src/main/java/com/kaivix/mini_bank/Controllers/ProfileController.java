99a5c41ded0455b098ef096ce0ee68e5619e7a14
kaivix <74367464+kaivix@users.noreply.github.com>
2023-12-24 21:18:40 +0400

    fix

--- src/main/java/com/kaivix/mini_bank/Controllers/ProfileController.java
+++ src/main/java/com/kaivix/mini_bank/Controllers/ProfileController.java
@@ -20,7 +20,7 @@ public class ProfileController {
 
     @GetMapping("/reg")
     public String reg(Model model){
-        model.addAttribute("users", new Users(pnum));
+        model.addAttribute("users", new Users());
         return "regis";
     }
 
